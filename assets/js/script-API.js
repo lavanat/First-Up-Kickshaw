52fcbf2eaf11b5e75a5c26370db6c0abb5427a05
Lavanya Natchiappan <lavee.natch@gmail.com>
2023-02-09 17:49:58 -0800

    format restaurant api url

--- assets/js/script-API.js
+++ assets/js/script-API.js
@@ -1,25 +1,42 @@
 var zipcode = "30024";
-var cuisine = ""
-var budget = ""
-var min-rating = ""
+var cuisine = "italian"
+var budget = 20
+var minRating = 4.5
 
-var requestLatLonURL = "http://api.openweathermap.org/geo/1.0/zip?zip=" + zipcode + ",US&appid=d37301983be8abf2d2f02d5906d87205";
 
-function getLatLong ()  {
-    fetch(requestLatLonURL)
+function getLatLong (zipcode)  {
+    var requestURL = "http://api.openweathermap.org/geo/1.0/zip?zip=" + zipcode + ",US&appid=d37301983be8abf2d2f02d5906d87205";
+    fetch(requestURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       var lat = data.lat
-      var long = data.lon
-      console.log(lat, long);
+      var lon = data.lon
+      getRestaurantApi (lat,lon)
     });
 }
 
-getLatLong ()
+function getRestaurantApi (lat,lon) {
+    var formattedLat = "&lat=" + lat;
+    var formattedLon = "&lng=" + lon;
+    var requestURL = "https://api.spoonacular.com/food/restaurants/search?apiKey=b2c2967dc5fd4b899471d38bc9f73a16" + formattedLat + formattedLon
+    if (cuisine !== "") {
+        formattedCuisine = "&cuisine=" + cuisine;
+        requestURL += formattedCuisine
+    }
+    if (budget !== "") {
+        formattedBudget = "&budget=" + budget;
+        requestURL += formattedBudget
+    }
+    if (minRating !== "") {
+        formattedRating = "&min-rating=" + minRating;
+        requestURL += formattedRating
+    }
+    console.log(requestURL)
+}
 
-
+getLatLong (zipcode)
 
 // const options = {
 // 	method: 'GET',
