d10b8ff5e230da454afa2fa542ad6b778f5fd468
Lavanya Natchiappan <lavee.natch@gmail.com>
2023-02-09 19:19:39 -0800

    call restaurant api and set textcontent of HTML elements

--- assets/js/script-API.js
+++ assets/js/script-API.js
@@ -13,14 +13,14 @@ function getLatLong (zipcode)  {
     .then(function (data) {
       var lat = data.lat
       var lon = data.lon
-      getRestaurantApi (lat,lon)
+      formatRestaurantURL (lat,lon)
     });
 }
 
-function getRestaurantApi (lat,lon) {
+function formatRestaurantURL (lat,lon) {
     var formattedLat = "&lat=" + lat;
     var formattedLon = "&lng=" + lon;
-    var requestURL = "https://api.spoonacular.com/food/restaurants/search?apiKey=b2c2967dc5fd4b899471d38bc9f73a16" + formattedLat + formattedLon
+    var requestURL = "https://api.spoonacular.com/food/restaurants/search?apiKey=2d16b5acc51c4165bb628b5ff87b47c8" + formattedLat + formattedLon
     if (cuisine !== "") {
         formattedCuisine = "&cuisine=" + cuisine;
         requestURL += formattedCuisine
@@ -33,19 +33,56 @@ function getRestaurantApi (lat,lon) {
         formattedRating = "&min-rating=" + minRating;
         requestURL += formattedRating
     }
-    console.log(requestURL)
+    RestaurantAPI (requestURL);
 }
+
+function RestaurantAPI (requestURL) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json'
+        }
+    };
+
+    fetch(requestURL,options)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        for (var i = 0; i < data.restaurants.length; i++) {
+            var nameID = "name-" + (i+1);
+            var picID = "pic-" + (i+1);
+            var addressID = "address-" + (i+1);
+            var monID = "mon-" + (i+1);
+            var tuesID = "tues-" + (i+1);
+            var wedID = "wed-" + (i+1);
+            var thurID = "thur-" + (i+1);
+            var friID = "fri-" + (i+1);
+            var satID = "sat-" + (i+1);
+            var sunID = "sun-" + (i+1);
+
+            document.getElementById(picID).textContent = data.restaurants[i].food_photos
+            document.getElementById(addressID).textContent = data.restaurants[i].address.street_addr + ", " + data.restaurants[i].address.city + ", " + data.restaurants[i].address.state
+            document.getElementById(monID).textContent = data.restaurants[i].local_hours.operational.Monday
+            document.getElementById(tuesID).textContent = data.restaurants[i].local_hours.operational.Tuesday
+            document.getElementById(wedID).textContent = data.restaurants[i].local_hours.operational.Wednesday
+            document.getElementById(thurID).textContent = data.restaurants[i].local_hours.operational.Thursday
+            document.getElementById(friID).textContent = data.restaurants[i].local_hours.operational.Friday
+            document.getElementById(satID).textContent = data.restaurants[i].local_hours.operational.Saturday
+            document.getElementById(sunID).textContent = data.restaurants[i].local_hours.operational.Sunday
+        }
+    });
+
+}
+
 
 getLatLong (zipcode)
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'content-type': 'application/json'
-// 	}
-// };
-
-// fetch('https://api.spoonacular.com/food/restaurants/search?apiKey=b2c2967dc5fd4b899471d38bc9f73a16&lat=40.7128&lng=-74.0060&distance=30', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-//   .catch(err => console.error(err));
+// restaurant name: name-1 - data.restaurants[i].name
+// restaurant pic: pic-1 - data.restaurants[i].food_photos
+// restaurant address: address-1 : 
+// data.restaurants[i].address.street_addr
+// data.restaurants[i].address.city
+// data.restaurants[i].address.state
+// restaurant hours: mon-1, tues-1, wed-1, thur-1, fri-1, sat-1, sun-1:
+// data.restaurants[i].local-hours.operational.Monday etc
