267493e58ebb643d9e9086c54ebf5a810797a73a
Lavanya Natchiappan <lavee.natch@gmail.com>
2023-02-09 17:16:17 -0800

    Added function to change zipcode to lat/lon

--- assets/js/script-API.js
+++ assets/js/script-API.js
@@ -1,12 +1,34 @@
+var zipcode = "30024";
+var cuisine = ""
+var budget = ""
+var min-rating = ""
 
-const options = {
-	method: 'GET',
-	headers: {
-		'content-type': 'application/json'
-	}
-};
+var requestLatLonURL = "http://api.openweathermap.org/geo/1.0/zip?zip=" + zipcode + ",US&appid=d37301983be8abf2d2f02d5906d87205";
 
-fetch('https://api.spoonacular.com/food/restaurants/search?apiKey=b2c2967dc5fd4b899471d38bc9f73a16&lat=40.7128&lng=-74.0060&distance=30', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-  .catch(err => console.error(err));
+function getLatLong ()  {
+    fetch(requestLatLonURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var lat = data.lat
+      var long = data.lon
+      console.log(lat, long);
+    });
+}
+
+getLatLong ()
+
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'content-type': 'application/json'
+// 	}
+// };
+
+// fetch('https://api.spoonacular.com/food/restaurants/search?apiKey=b2c2967dc5fd4b899471d38bc9f73a16&lat=40.7128&lng=-74.0060&distance=30', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+//   .catch(err => console.error(err));
